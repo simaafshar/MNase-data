04c945b3fb0080601531508b7a362b8e913233a7
yunhuip <yunhuipengys@gmail.com>
2020-03-24 13:04:01 -0400

    Create positioning_score.sh

---
+++ #!/bin/bash module load bedops/2.4.35 module load crossmap/positioning_score.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+module load bedops/2.4.35
+module load crossmap/0.3.3
+module load bedtools/2.27.1
+for ((i=1;i<=22;i++));
+do
+#wig2bed < chr${i}.wig > chr${i}.bed
+# Filter the fragments with 0 reads
+#awk '{OFS="\t"}{ if($5>0){print $1, $2, $3, $5}}' chr${i}.bed > chr${i}_filtered.bed
+#crossmap bed hg18ToHg19.over.chain chr${i}.bed chr${i}_hg19.bed
+bedtools intersect -a chr${i}_hg19.bed -b mnase_mids_stringency_hg19_sort.bed > chr${i}_hg19_score.bed
+done
