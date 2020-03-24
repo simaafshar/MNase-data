5238729eee4f3329c17b40a3d6a3bde3879318e5
yunhuip <yunhuipengys@gmail.com>
2020-03-24 14:31:50 -0400

    Update nucleosome_mappable.sh

--- nucleosome_mappable.sh
+++ nucleosome_mappable.sh
@@ -12,3 +12,7 @@ cat mnase_mids_stringency_hg19_sort.bed | sort -k1,1 -k2,2n | \
     intersectBed -a stdin -b wgEncodeCrgMapabilityAlign36mer_score1.bed -wo | \
     awk '{OFS="\t";}{print $1, $2+73, $3-73, $4, $5, $6, $7, $8, $9, $10, $11,$12}' |  \
     sort -u > dyads_mappable.bed
+
+## find the nuclesome with at least 75% of the sequence wityh score =1
+python3 Nucleosome_mappable.py
+awk 'BENGIN{FS='\t'}{if($5>=0.75) print $0}' dyads_mappability_score1_ratio_all.bed > dyads_mappability_score1_ratio_0.75.bed
