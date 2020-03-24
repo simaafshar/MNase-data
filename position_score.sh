53e46e93a7783ef384c4e31b064b3a51869b182c
yunhuip <yunhuipengys@gmail.com>
2020-03-24 13:26:42 -0400

    Update position_score.sh

--- position_score.sh
+++ position_score.sh
@@ -5,8 +5,9 @@ module load bedtools/2.27.1
 for ((i=1;i<=22;i++));
 do
 wig2bed < chr${i}.wig > chr${i}.bed
-# Filter the fragments with 0 reads
+# Filter out the fragments with 0 reads
 awk '{OFS="\t"}{ if($5>0){print $1, $2, $3, $5}}' chr${i}.bed > chr${i}_filtered.bed
-crossmap bed hg18ToHg19.over.chain chr${i}.bed chr${i}_hg19.bed
+crossmap bed hg18ToHg19.over.chain chr${i}.bed chr${i}_hg19.bed 
+## intersect the dyad position with positioning socre
 bedtools intersect -a chr${i}_hg19.bed -b mnase_mids_stringency_hg19_sort.bed > chr${i}_hg19_score.bed
 done
